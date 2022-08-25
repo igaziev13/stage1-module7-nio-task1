786046a7ba63f4af85b11ccec2be743af771dfc8
IGAZIEV <islam.gaziev@gmail.com>
2022-08-25 13:58:23 +0500

    Task NIO. Attempt 1.

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,26 @@
 package com.epam.mjc.nio;
 
-import java.io.File;
+import java.io.*;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        String name = "";
+        int age = 0;
+        String email = "";
+        long phone = 0L;
+        try(java.io.FileReader fl = new java.io.FileReader(file)) {
+            BufferedReader reader = new BufferedReader(fl);
+            name = reader.readLine().split(":")[1].trim();
+            age = Integer.parseInt(reader.readLine().split(":")[1].trim());
+            email = reader.readLine().split(":")[1].trim();
+            phone = Long.parseLong(reader.readLine().split(":")[1].trim());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new Profile(name, age, email, phone);
     }
 }
